6d32a482426d07e7a2f1f3d83a83132f4b297a10
jusselman <usselmanjosh@gmail.com>
2018-07-23 12:58:52 -0700

    popup window magic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var popupDiv = document.getElementById('popup_bg');
+
+function openPopUpMenu() {
+  popupDiv.style.display = 'block';
+}
+
+function closePopupMenu() {
+  popupDiv.style.display = 'none';
+}
